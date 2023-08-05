001a2368ae0935ff0596db922181a9ca34cbe7e1
lilbaby1 <96513042+lilbaby1@users.noreply.github.com>
2023-08-05 12:01:40 +0300

    Changed BASE_URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3500/";
+const BASE_URL = "https://blog-backend-3wrg.onrender.com/"; // "http://localhost:3500/";
 
 export default axios.create({
   baseURL: BASE_URL,
